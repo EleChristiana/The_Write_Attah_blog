1064576d1abc519b4ad4d6ef3972996ad64ca8f1
TianaEle <o.christiana@tajnetworkafrica.com>
2025-07-11 10:23:26 +0100

    dashboard and added toggle icon functionality

--- src/app/reuseable/sidebar/sidebar.component.ts
+++ src/app/reuseable/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit, Renderer2 } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
@@ -9,14 +9,37 @@ import { RouterLink, RouterModule } from '@angular/router';
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+ 
   sidebarActive = false;
-  
+  themeIcon: string = 'fa fa-moon';
+
+
+
+   ngOnInit(): void {
+       this.renderer.addClass(document.body, 'light-theme');
+  }
+
+  constructor( private renderer: Renderer2){
+    
+  }
 
   toggleSidebar() {
     this.sidebarActive = !this.sidebarActive;
   }
 
 
- 
+  toggleTheme(): void {
+    const isLight = document.body.classList.contains('light-theme');
+
+    if (isLight) {
+      this.renderer.removeClass(document.body, 'light-theme');
+      this.renderer.addClass(document.body, 'dark-theme');
+      this.themeIcon = 'fa fa-sun';
+    } else {
+      this.renderer.removeClass(document.body, 'dark-theme');
+      this.renderer.addClass(document.body, 'light-theme');
+      this.themeIcon = 'fa fa-moon';
+    }
+  }
 }
