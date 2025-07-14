c0478727b2ce7bfd46e8c2eca299e30314587a10
TianaEle <o.christiana@tajnetworkafrica.com>
2025-07-14 12:44:00 +0100

    removed environments folder from gitignore to enable hosting

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  ownerUid: 'Dh7ksCivITfijLD9S5gcZcWexx02',
+    cloudinary: {
+      cloudinaryUploadUrl: 'https://api.cloudinary.com/v1_1/dwlzmvqhv/image/upload',
+    uploadPreset: 'angular_unsigned'
+  }
+};
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+ 
+  ownerUid: 'dev-owner-uid',
+
+   cloudinary: {
+    cloudinaryUploadUrl: 'https://api.cloudinary.com/v1_1/dwlzmvqhv/image/upload',
+    uploadPreset: 'angular_unsigned'
+  }
+};
+
+
